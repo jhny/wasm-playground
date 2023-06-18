95bd3e5492e7cac6709ad4d45d808acbc14a4bc7
Johnny Eradus <j.eradus@digitalechecklisten.nl>
2023-06-18 13:18:15 +0200

    First commit

---
+++ src/lib.rs
@@ -0,0 +1,84 @@
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{console, Request, RequestInit, RequestMode, Response};
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    let x = 0;
+
+    let x = x + 12;
+    let s = x.to_string();
+
+    let mut xs = Vec::new();
+
+    xs.push(1);
+    xs.push(2);
+    xs.push(3);
+    xs.push(4);
+
+    for x in xs {
+        let s = x.to_string();
+        console::log_1(&JsValue::from_str(&s));
+    }
+
+    console::log_1(&JsValue::from_str(&s));
+}
+
+#[wasm_bindgen]
+pub async fn get_repo(repo: String) -> Result<JsValue, JsValue> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors);
+
+    let url = format!("https://api.github.com/repos/{}/branches/master", repo);
+
+    let request = Request::new_with_str_and_init(&url, &opts)?;
+
+    request
+        .headers()
+        .set("Accept", "application/vnd.github.v3+json")?;
+
+    let window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+
+    // `resp_value` is a `Response` object.
+    assert!(resp_value.is_instance_of::<Response>());
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    // Convert this other `Promise` into a rust `Future`.
+    let json = JsFuture::from(resp.json()?).await?;
+
+    // Send the JSON response back to JS.
+    return Ok(json);
+}
+
+// Called by our JS entry point to run the example
+#[wasm_bindgen(start)]
+fn run() -> Result<(), JsValue> {
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    val.set_text_content(Some("Hello from Rust!"));
+
+    body.append_child(&val)?;
+
+    console::log_1(&JsValue::from_str("Hello world!"));
+
+    Ok(())
+}
